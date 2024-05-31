02843afacbab14569a8f028bea7c4266331f5a55
Rebecca <rebecca90469@gmail.com>
2024-05-31 18:34:17 -0400

    Setup for project

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]); 
+
+  const transition = (newMode) => {
+    setHistory(prev => [...prev, newMode])
+  }
+
+  const back = () => {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length -1], transition, back };
+}
